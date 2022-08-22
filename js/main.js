d950bc0a9f6bb83387ed0585fad551c02f13eb55
AntonyUre <ureta.art@hotmail.com>
2022-08-22 14:13:01 -0500

    final

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 const API_KEY = "e8d8e36405e49666a199b1545524fbe5";
 // link de api
 const WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather";
-//link de google maps
+// link de google maps
 const MAPS_URL = "https://api.openweathermap.org/data/2.5/forecast";
 
 // Contenedores
@@ -11,14 +11,15 @@ const form = document.getElementById("search-form");
 const searchBox = document.getElementById("searchbox");
 const boxMap = document.querySelector(".right");
 
-async function getWeather() {
+async function getWeather(city) {
   return new Promise((resolve, reject) => {
     axios
       .get(
-        `${WEATHER_URL}?q=${searchBox.value}&lang=es&appid=${API_KEY}&units=metric`
+        `${WEATHER_URL}?q=${
+          searchBox.value || city
+        }&lang=es&appid=${API_KEY}&units=metric`
       )
       .then((response) => {
-        console.log(response);
         resolve(response);
       })
       .catch((error) => {
@@ -28,19 +29,11 @@ async function getWeather() {
   });
 }
 
-// document.addEventListener('keypress', function(event){
-// 	if(event.key){
-//     console.log(event.key)
-// 	}
-// });
-
-
 function onSubmit(event) {
   event.preventDefault();
   if (searchBox.value === "") {
     alert("esta vacio");
-    getLocation(); //latitud(horizontal) y longitud(vertical)
-    //notificacione();
+    getLocation();
   }
   let value = searchBox.value;
   console.log(value);
@@ -57,11 +50,10 @@ function fecha(startPosition) {
 }
 
 function printMap(res) {
-  console.log("printMap");
   let lat = res.data.coord.lat;
-  console.log(lat);
+  // console.log(lat);
   let lon = res.data.coord.lon;
-  console.log(lon);
+  // console.log(lon); 
   let ifrm = document.createElement("iframe");
   ifrm.setAttribute(
     "src",
@@ -72,34 +64,58 @@ function printMap(res) {
 }
 
 // Imprimir la informacion del clima
-function printCard(res) {
+async function printCard(city) {
+  let res = await getWeather(city);
   let timestamp = res.data.dt;
   let date = new Date(timestamp * 1000);
   let day = date.getDate();
   let month = date.getMonth();
   let year = date.getFullYear();
   let hour = date.getHours();
-  let min = date.getMinutes();
+  let min = date.getMinutes()
+
+  if (res.data.name.length >= 1) {
+    notificacionTrue()
+  } else { 
+    notificacionFalse()
+  }
+  // <div><p>${day + "/" + month + "/" + year + " " + hour + ":" + min}</p></div>
   // let sec = date.getSeconds();
   // console.log(
   //   day + "-" + month + "-" + year + " " + hour + ":" + min + ":" + sec
   // );
 
   temp.innerHTML = `
-  <div class="card__item">
+  <div class="card__card">
     <div class="card__img-container">
-    <div><h2>${res.data.main.temp}° C</h2></div>
-    <div><h2>${
-      day + "-" + month + "-" + year + " " + hour + ":" + min
-    }</h2></div>
+    <div class="card__card--data">
+    <h2>${res.data.main.temp}° C</h2>
+    <h3>${res.data.name} - ${res.data.sys.country}</h3>
+    </div>
+    
       <img class="card__logo" src="http://openweathermap.org/img/wn/${
         res.data.weather[0].icon
       }@2x.png" alt="Logo OpenWeather" />
     </div>
+
     <div class="card__text-container">
-      <div><h2>${res.data.name} - ${res.data.sys.country}</h2></div>
-      <div><h2>${res.data.main.humidity}</h2></div>
-      <div><h2>Temperatura${res.data.weather[0].description}</h2></div>
+      
+      <div class="card__card--items">
+      <img src="../img/termometro.png">
+      <p>${res.data.main.temp_min} min</p>
+      </div>
+      <div class="card__card--items">
+      <img src="../img/termometro.png">
+      <p>${res.data.main.temp_max} max</p>
+      </div>
+      <div class="card__card--items">
+      <img src="../img/wind.png">
+      <p>${res.data.wind.speed} km/h</p>
+      </div>
+      <div class="card__card--items">
+      <img src="../img/nublado.png">
+      <p>${res.data.weather[0].description}</p>
+      </div>
     </div>
   </div>`;
   printMap(res);
@@ -109,25 +125,20 @@ const time = async () => {
   const res = await getWeather();
   printCard(res);
 };
-
-// Prueba de mapa
 function mapaTest(x, y, startPosition) {
-  console.log("Se imprimio el mapa");
-  console.log(startPosition + " Funciona la fecha");
   searchBox.value = startPosition;
   let res = startPosition;
   printCard(res);
   let ifrm = document.createElement("iframe");
-  // ifrm.setAttribute(
-  //   "src",
-  //   `https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d62414.9511018945!2d${y}!3d${x}!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses-419!2spe!4v1658433513389!5m2!1ses-419!2spe`
-  // );
+  ifrm.setAttribute(
+    "src",
+    `https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d62414.9511018945!2d${y}!3d${x}!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses-419!2spe!4v1658433513389!5m2!1ses-419!2spe`
+  );
   boxMap.innerHTML = "";
   boxMap.appendChild(ifrm);
 }
 
 // Inicio de obtener ubicación
-
 const getLocation = async () => {
   let urlApi = "https://ipinfo.io?token=849ad9412151af";
   let response = await axios
@@ -136,42 +147,38 @@ const getLocation = async () => {
     .catch((error) => {
       console.log(error);
     });
-  // console.log(response.city);
-  // console.log(response.city);
   const startPosition = response.city;
-  // fecha(startPosition);
   let cor = response.loc.split(",");
   const x = cor[0];
   const y = cor[1];
-  console.log(x + " + " + y);
   mapaTest(x, y, startPosition);
   onSubmit();
+  
 };
 // Fin de obtener ubicación
 
-// Star sweet alert
-function notificacione() {
-  setTimeout(() => {
-    Swal.fire({
-      position: "center",
-      icon: "success",
-      title: "Se detecto tu ubicación",
-      showConfirmButton: false,
-      timer: 2000,
-    });
-  }, 2000);
+// Start sweet alert
+function notificacionTrue() {
+  Swal.fire({
+    position: "center",
+    icon: "success",
+    title: "Se detecto tu ubicación",
+    showConfirmButton: false,
+    timer: 2000,
+  });
 }
-//En sweet alert
+function notificacionFalse() {
+  Swal.fire({
+    position: "center",
+    icon: "error",
+    title: "Se detecto tu ubicación",
+    showConfirmButton: false,
+    timer: 2000,
+  });
+}
+//End sweet alert
 
 window.onload = function () {
   // alert('hola')
-  //getLocation(); //latitud(horizontal) y longitud(vertical)
-  //notificacione();
-  form.submit
-
-  // if (searchBox.value === "") {
-  //   getLocation(); //latitud(horizontal) y longitud(vertical)
-  //   notificacione();
-  // } else {
-  // }
+  getLocation();
 };
